f622619ca88d6a4c1b0457e209a2bb067bb19550
Avinash Kumar <avinashkumarsinha01@gmail.com>
2024-12-15 11:53:47 +0530

    Update code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Homepage from './component/HomePage';
+import Dashboard from './component/Dashboard';  
+import TaskList from './component/TaskList';
+
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/tasklist" element={<TaskList/>} />
+      </Routes>
+    </Router>
   );
 }
 
